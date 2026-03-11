0ee8da521be591544366797d4645ee37ebb9689f
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2026-03-11 09:37:15 +0700

    Update RecentProjects.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -56,7 +56,7 @@ const RecentProjects = () => {
             const globalIndex = startIndex + index;
             return (
               <div
-                className="lg:min-h-[20rem] h-[18rem] flex items-center justify-center sm:w-96 w-[80vw]"
+                className="lg:min-h-[25rem] h-[18rem] flex items-center justify-center sm:w-96 w-[80vw]"
                 key={item.id}
               >
                 <ExpandableProjectCard
